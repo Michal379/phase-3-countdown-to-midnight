d963216e0f0f66b8bb13439ae5466fb69586fd81
Michal <khakaimic4@gmail.com>
2023-05-22 06:44:51 +0300

    completed practice

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
-end
+   #countdown
+def countdown(integer)
+    while integer > 0
+      puts "#{integer} SECOND(S)!"
+      integer -= 1
+      sleep 1
+    end
+    "HAPPY NEW YEAR!"
+  end
+  
+       #countdown_with_sleep
+def countdown_with_sleep(integer)
+    while integer > 0
+      puts "#{integer} SECOND(S)!"
+      integer -= 1
+      sleep 1     # pauses for 1sec each trip
+    end
+    "HAPPY NEW YEAR!"
+  end
+  
